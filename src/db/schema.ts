d0312fa2153ac9704a3085bbf2f11110790201e5
brendaAndersen <52722720+brendaAndersen@users.noreply.github.com>
2024-09-13 11:41:47 +0000

    Pending changes exported from your codespace

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -12,7 +12,7 @@ export const goals = pgTable("goals", {
 
 export const goalCompletions = pgTable('goal_completions', {
 	id: text('id').primaryKey(),
-	goaldId: text('goal_id').references(() => goals.id).notNull(),
+	goalId: text('goal_id').references(() => goals.id).notNull(),
 	createdAt: timestamp("created_at", { withTimezone: true })
 		.notNull()
 		.defaultNow(),
--- src/useCases/createGoal.ts
+++ src/useCases/createGoal.ts
@@ -2,19 +2,21 @@ import { db } from "../db"
 import { goals } from "../db/schema"
 
 interface CreateGoalRequest {
-    title: string,
-    desiredWeeklyFrequency: number
+  title: string
+  desiredWeeklyFrequency: number
 }
 
-export async function createGoal({ title, desiredWeeklyFrequency } : CreateGoalRequest){
-    const result = await db.insert(goals).values({
-        title,
-        desiredWeeklyFrequency
-    }).returning()
-
-    const goal = result[0];
-
-    return {
-        goal,
-    }
+export async function createGoal({
+  title,
+  desiredWeeklyFrequency,
+}: CreateGoalRequest) {
+  const [goal] = await db
+    .insert(goals)
+    .values({
+      title,
+      desiredWeeklyFrequency,
+    })
+    .returning()
+''
+  return { goal }
 }
